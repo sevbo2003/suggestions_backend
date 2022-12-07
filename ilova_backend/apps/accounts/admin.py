4c52b9c5eec6fab5a8d8475e70a5fb5dd22e39a8
Abdusamad Malikov <sevbofx@gmail.com>
2022-12-07 10:13:38 -0500

    `admin` added to user

--- ilova_backend/apps/accounts/admin.py
+++ ilova_backend/apps/accounts/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext_lazy as _
+from phonenumber_field.formfields import PhoneNumberField
 
-# Register your models here.
+from .models import CustomUser
+
+
+# class CustomUserAdmin(UserAdmin):
+    # fieldsets = (
+    #     (None, {'fields': ('username', 'password')}),
+    #     (_('Permissions'), {
+    #         'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
+    #     }),
+    #     (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    # )
+    # list_display = ('username', 'is_verified', 'is_staff')
+    # search_fields = ('username',)
+    # ordering = ('username',)
+    # filter_horizontal = ('groups', 'user_permissions',)
+    # list_filter = ('is_staff', 'is_superuser', 'is_active', 'groups', 'is_verified')
+
+
+admin.site.register(CustomUser)
+            
