d48660855669bd31b48d45751c3c65f222dfb0eb
Abdusamad Malikov <sevbofx@gmail.com>
2022-12-08 16:38:50 -0500

    `problem` `save` signal updated

--- ilova_backend/apps/suggestions/models.py
+++ ilova_backend/apps/suggestions/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from core.geo_finder import get_location
 import uuid
 
 User = get_user_model()
@@ -10,6 +11,10 @@ class ProblemType(models.Model):
 
     def __str__(self):
         return self.name
+    
+    class Meta:
+        verbose_name = "Ariza turi"
+        verbose_name_plural = "Ariza turlari"
 
 
 class Status(models.TextChoices):
@@ -30,3 +35,16 @@ class Problem(models.Model):
     status = models.CharField(max_length=100, choices=Status.choices, default=Status.PENDING)
     lon = models.DecimalField(max_digits=9, decimal_places=6)
     lat = models.DecimalField(max_digits=9, decimal_places=6)
+
+    def __str__(self):
+        return str(self.id)
+    
+    class Meta:
+        ordering = ["-date"]
+        verbose_name = "Ariza"
+        verbose_name_plural = "Arizalar"
+    
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        self.city, self.district = get_location(str(self.lon), str(self.lat))
+        super().save(*args, **kwargs)
