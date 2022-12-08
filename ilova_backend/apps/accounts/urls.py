9de2cfae18d89bceedfee64c01a65e0414290473
Abdusamad Malikov <sevbofx@gmail.com>
2022-12-08 15:11:40 -0500

    sms `urls` changed

--- ilova_backend/apps/accounts/urls.py
+++ ilova_backend/apps/accounts/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from .views import GenerateOTP, ValidateOTP, TestPermission
 
 urlpatterns = [
-    url(r'^generate/$', GenerateOTP.as_view(), name="generate"),
-    url(r'^validate/$', ValidateOTP.as_view(), name="validate"),
+    url(r'^sms-send/$', GenerateOTP.as_view(), name="generate"),
+    url(r'^sms-verify/$', ValidateOTP.as_view(), name="validate"),
     url(r'^test/$', TestPermission.as_view(), name="test"),
 ]
