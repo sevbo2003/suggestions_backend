988feaa92184b4a6c45de3625aca643871a34b61
Abdusamad Malikov <sevbofx@gmail.com>
2022-12-10 10:10:14 -0500

    `Location` model created

--- ilova_backend/apps/suggestions/models.py
+++ ilova_backend/apps/suggestions/models.py
@@ -23,6 +23,14 @@ class Status(models.TextChoices):
     FAKE = "Yolg'on"
 
 
+class Location(models.Model):
+    lon = models.DecimalField(max_digits=24, decimal_places=22)
+    lat = models.DecimalField(max_digits=24, decimal_places=22)
+
+    def __str__(self) -> str:
+        return f"{self.lon} + {self.lat}"
+
+
 class Problem(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_problems")
     city = models.CharField(max_length=100, null=True, blank=True)
@@ -32,8 +40,7 @@ class Problem(models.Model):
     problem_types = models.ManyToManyField(ProblemType, related_name="problems")
     description = models.CharField(max_length=1000)
     status = models.CharField(max_length=100, choices=Status.choices, default=Status.PENDING)
-    lon = models.DecimalField(max_digits=9, decimal_places=6)
-    lat = models.DecimalField(max_digits=9, decimal_places=6)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE, related_name="problems")
 
     def __str__(self):
         return str(self.id)
@@ -44,5 +51,5 @@ class Problem(models.Model):
         verbose_name_plural = "Arizalar"
     
     def save(self, *args, **kwargs):
-        self.city, self.district = get_location(str(self.lon), str(self.lat))
+        self.city, self.district = get_location(str(self.location.lon), str(self.location.lat))
         super().save(*args, **kwargs)
