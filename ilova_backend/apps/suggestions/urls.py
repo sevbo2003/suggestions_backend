ff956cc4613d3e657903a4a94f57c6886939d8ab
Abdusamad Malikov <sevbofx@gmail.com>
2022-12-10 10:11:25 -0500

    `Location` urls

--- ilova_backend/apps/suggestions/urls.py
+++ ilova_backend/apps/suggestions/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from apps.suggestions.views import ProblemTypeViewSets, ProblemViewSets
+from apps.suggestions.views import ProblemTypeViewSets, ProblemViewSets, LocationViewSet
 
 router = DefaultRouter()
 
+router.register('locations', LocationViewSet, basename='location')
 router.register('problems', ProblemViewSets, basename='problems')
 router.register('problem_types', ProblemTypeViewSets, basename='problem-types')
 
