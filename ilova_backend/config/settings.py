1788de8fb6f323cf28f38cdedb9b812463c2a47e
Abdusamad Malikov <sevbofx@gmail.com>
2022-12-10 03:43:29 -0500

    `settings.py` updated

--- ilova_backend/config/settings.py
+++ ilova_backend/config/settings.py
@@ -163,10 +163,11 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         # "rest_framework_simplejwt.authentication.JWTAuthentication",
         # 'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
     ),
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'DEFAULT_PAGINATION_CLASS': 'core.paginations.CustomPagination',
     'PAGE_SIZE': 100,
 }
 AUTH_USER_MODEL = 'accounts.PhoneNumberAbstactUser'
