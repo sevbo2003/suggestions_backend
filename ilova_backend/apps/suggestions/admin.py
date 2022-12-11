ca998e1446363c614bfc6dd835049173e05fa169
Abdusamad Malikov <sevbofx@gmail.com>
2022-12-11 11:14:13 -0500

    `ProblemImageAdmin` created

--- ilova_backend/apps/suggestions/admin.py
+++ ilova_backend/apps/suggestions/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from apps.suggestions.models import ProblemType, Problem, Location
+from apps.suggestions.models import ProblemType, Problem, Location, ProblemImages
 
 
 @admin.register(ProblemType)
@@ -8,12 +8,26 @@ class ProblemTypeAdmin(admin.ModelAdmin):
     search_fields = ("name",)
 
 
+@admin.register(ProblemImages)
+class ProblemImagesAdmin(admin.ModelAdmin):
+    list_display = ("id", "problem", "image")
+    search_fields = ("id", "problem", "image")
+    list_filter = ("problem",)
+    readonly_fields = ("id", "problem", "image")
+
+
+class ProblemImagesInline(admin.TabularInline):
+    model = ProblemImages
+    extra = 1
+
+
 @admin.register(Problem)
 class ProblemAdmin(admin.ModelAdmin):
     list_display = ("id", "user", "city", "district", "date", "status")
     search_fields = ("id", "user", "city", "district", "date", "status")
     list_filter = ("status",)
     readonly_fields = ("id", "city", "district", "date")
+    inlines = [ProblemImagesInline]
 
 
 @admin.register(Location)
