4cad1da2212548bc3ea07fefdbeb7fb348874445
Abdusamad Malikov <sevbofx@gmail.com>
2022-12-14 15:54:41 -0500

    Main `routing` as appication

---
+++ ilova_backend/config/routing.py
@@ -0,0 +1,20 @@
+import os
+import django
+from .middlewares import JWTAuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+
+from django.core.asgi import get_asgi_application
+
+from apps.chat.routing import websocket_urlpatterns
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chat_app.settings')
+django.setup()
+
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": JWTAuthMiddlewareStack(
+        URLRouter(
+            websocket_urlpatterns
+        )
+    )
+})
