3a52716ed50685f2e6d868bf5b058caa7389b0f5
Abdusamad Malikov <sevbofx@gmail.com>
2022-12-14 15:55:20 -0500

    `Message` model created

--- ilova_backend/apps/chat/models.py
+++ ilova_backend/apps/chat/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from apps.suggestions.models import Problem
 
-# Create your models here.
+
+class Message(models.Model):
+    from_problem_user = models.ForeignKey(Problem, on_delete=models.CASCADE, related_name='from_user')
+    message = models.CharField(max_length=1000)
+    is_read = models.BooleanField(default=False)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.message[:20]
+    
+    class Meta:
+        ordering = ('-date',)
+    
