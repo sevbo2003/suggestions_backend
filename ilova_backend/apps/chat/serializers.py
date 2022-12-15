6fd82df6497d01adff6a5d7e17e44dfad0c47e52
Abdusamad Malikov <sevbofx@gmail.com>
2022-12-15 09:23:50 -0500

    `ChatProblemSerializer` created

--- ilova_backend/apps/chat/serializers.py
+++ ilova_backend/apps/chat/serializers.py
@@ -1,11 +1,33 @@
 from rest_framework import serializers
-from apps.chat.models import Message
+from apps.chat.models import Message, ChatProblem
+
+
+class ChatProblemSerializer(serializers.ModelSerializer):
+    problem = serializers.SerializerMethodField()
+
+    class Meta:
+        model = ChatProblem
+        fields = ('id', 'problem', 'created_at')
+        read_only_fields = ('created_at',)
+    
+    def get_problem(self, obj):
+        return {
+            'id': obj.problem.id,
+            'title': obj.problem.city + ' ' + obj.problem.district
+        }
+    
+    def create(self, validated_data):
+        problem = validated_data.pop('problem')
+        chat_problem = ChatProblem.objects.create(**validated_data)
+        chat_problem.problem = problem
+        chat_problem.save()
+        return chat_problem     
 
 
 class MessageSerializer(serializers.ModelSerializer):
     class Meta:
         model = Message
-        fields = ('id', 'from_problem_user', 'message', 'is_read', 'date')
+        fields = ('id', 'chat_problem', 'message', 'is_read', 'date')
     
     def create(self, validated_data):
         message = Message.objects.create(**validated_data)
