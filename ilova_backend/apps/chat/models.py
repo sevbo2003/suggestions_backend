b92a667b6e5bd574c9c2463d14a5b364c6e173f4
Abdusamad Malikov <sevbofx@gmail.com>
2022-12-15 09:21:53 -0500

    `ChatProblem` model created and linked to `Message` model

--- ilova_backend/apps/chat/models.py
+++ ilova_backend/apps/chat/models.py
@@ -2,8 +2,19 @@ from django.db import models
 from apps.suggestions.models import Problem
 
 
+class ChatProblem(models.Model):
+    problem = models.ForeignKey(Problem, on_delete=models.CASCADE, related_name='chat_problem')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.problem.description[:20]
+    
+    class Meta:
+        ordering = ('-created_at',)
+
+
 class Message(models.Model):
-    from_problem_user = models.ForeignKey(Problem, on_delete=models.CASCADE, related_name='from_user')
+    chat_problem = models.ForeignKey(ChatProblem, on_delete=models.CASCADE, related_name='messages')
     message = models.CharField(max_length=1000)
     is_read = models.BooleanField(default=False)
     date = models.DateTimeField(auto_now_add=True)
@@ -13,4 +24,4 @@ class Message(models.Model):
     
     class Meta:
         ordering = ('-date',)
-    
+        
