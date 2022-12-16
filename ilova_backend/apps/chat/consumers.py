a1262ad179f083bcbb37c2b47a3869e645b3e46f
Abdusamad Malikov <sevbofx@gmail.com>
2022-12-16 05:54:50 -0500

    `Consumers` updated

--- ilova_backend/apps/chat/consumers.py
+++ ilova_backend/apps/chat/consumers.py
@@ -3,23 +3,30 @@ import json
 from channels.generic.websocket import AsyncWebsocketConsumer
 from asgiref.sync import sync_to_async
 
-from .models import Message
+from apps.chat.models import Message, ChatProblem, SenderType
+from apps.suggestions.models import Problem
 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        self.problem_id = self.scope['url_route']['kwargs']['problem_id']
+        self.chat_id = self.scope['url_route']['kwargs']['chat_id']
         user = self.scope.get('user', False)
-        print(user.id)
-        self.problem_id_name = 'problem_%s' % self.problem_id
+        chat = await self.get_chat(self.chat_id)
+        chat_user = await self.get_chat_user(chat.problem_id)
+        if not user.is_authenticated or not chat:
+            await self.close()
+        if chat_user != user:
+            if not user.is_staff:
+                await self.close()
+        self.chat_id_name = 'chat_%s' % self.chat_id
         await self.channel_layer.group_add(
-            self.problem_id_name,
+            self.chat_id_name,
             self.channel_name
         )
         await self.accept()
     
     async def disconnect(self, close_code):
         await self.channel_layer.group_discard(
-            self.problem_id_name,
+            self.chat_id_name,
             self.channel_name
         )
     
@@ -28,35 +35,64 @@ class ChatConsumer(AsyncWebsocketConsumer):
         message = text_data_json['message']
         user = self.scope.get('user', False)
         is_read = False
-        problem_id = self.problem_id
+        chat_id = self.chat_id
+        if user.is_staff:
+            sender = 'admin'
+        else:
+            sender = 'user'
         if user.is_authenticated:
-            await self.save_message(message, user)
+            await self.save_message(message, chat_id, sender)
             await self.channel_layer.group_send(
-                self.problem_id_name,
+                self.chat_id_name,
                 {
                     'type': 'chat_message',
                     'message': message,
                     'user': user.username,
                     'is_read': is_read,
-                    'problem_id': problem_id
+                    'chat_id': int(chat_id),
+                    'sender': sender
                 }
             )
+
+    @sync_to_async
+    def get_chat(self, chat_id):
+        try:
+            chat = ChatProblem.objects.get(id=chat_id)
+        except ChatProblem.DoesNotExist:
+            chat = None
+        return chat
+    
+    @sync_to_async
+    def get_chat_user(self, problem_id):
+        try:
+            chat = Problem.objects.get(id=problem_id)
+            return chat.user
+        except ChatProblem.DoesNotExist:
+            chat = None
+        return chat
 
     async def chat_message(self, event):
         message = event['message']
         user = event['user']
         is_read = event['is_read']
-        problem_id = event['problem_id']
+        chat_id = event['chat_id']
+        sender = event['sender']
         await self.send(text_data=json.dumps({
             'message': message,
             'user': user,
             'is_read': is_read,
-            'problem_id': problem_id
+            'chat_id': chat_id,
+            'sender': sender
         }))
     
     @sync_to_async
-    def save_message(self, message, problem_id):
+    def save_message(self, message, chat_id, sender):
+        try:
+            chat = ChatProblem.objects.get(id=chat_id)
+        except ChatProblem.DoesNotExist:
+            chat = ChatProblem.objects.create(problem_id=chat_id)
         Message.objects.create(
             message=message,
-            from_problem_user=problem_id
+            chat_problem=chat,
+            sender=SenderType[sender.upper()]
         )
