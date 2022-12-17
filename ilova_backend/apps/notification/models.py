370b82768948216ad2f8a207f4c88dbf2fcf8caf
Abdusamad Malikov <sevbofx@gmail.com>
2022-12-17 20:05:48 +0500

    `Notification` model created

--- ilova_backend/apps/notification/models.py
+++ ilova_backend/apps/notification/models.py
@@ -7,3 +7,19 @@ class Mahalla(models.Model):
 
     def __str__(self):
         return self.district + " " + self.mahalla
+
+
+class Notification(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.ImageField(upload_to='notification/images/', blank=True, null=True)
+    mahalla = models.ManyToManyField(Mahalla, related_name="notification")
+    date = models.DateTimeField()
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        ordering = ['-date']
+        verbose_name_plural = "Xabarnomalar"
+        verbose_name = "Xabarnoma"
