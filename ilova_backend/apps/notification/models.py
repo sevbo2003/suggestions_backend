0eafa3fce545eeb478dae8f448759227210c8157
Abdusamad Malikov <sevbofx@gmail.com>
2022-12-17 17:40:59 +0500

    `Mahalla` model created

--- ilova_backend/apps/notification/models.py
+++ ilova_backend/apps/notification/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Mahalla(models.Model):
+    district = models.CharField(max_length=100)
+    mahalla = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.district + " " + self.mahalla
