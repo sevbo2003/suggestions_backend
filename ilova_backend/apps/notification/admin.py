f1b010e1ada885b0bf5ba8e92fcd60616f393b18
Abdusamad Malikov <sevbofx@gmail.com>
2022-12-17 18:21:03 +0500

    `MahallaAdmin` created

--- ilova_backend/apps/notification/admin.py
+++ ilova_backend/apps/notification/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from apps.notification.models import Mahalla
 
-# Register your models here.
+
+@admin.register(Mahalla)
+class MahallaAdmin(admin.ModelAdmin):
+    list_display = ["district", "mahalla"]
+    list_filter = ["district"]
+    search_fields = ["district", "mahalla"]
