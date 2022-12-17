63de4071fe2f5e5fda2ad8235e292c2395c750f8
Abdusamad Malikov <sevbofx@gmail.com>
2022-12-17 20:13:09 +0500

    `NotificationViewSet`

--- ilova_backend/apps/notification/views.py
+++ ilova_backend/apps/notification/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from rest_framework import permissions
+from apps.notification.models import Notification
+from apps.notification.serializers import NotificationSerializer
 
-# Create your views here.
+
+class NotificationViewSet(ModelViewSet):
+    queryset = Notification.objects.all()
+    serializer_class = NotificationSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        if self.request.user.is_superuser:
+            return self.queryset
+        return self.queryset.filter(mahalla__mahalla=self.request.user.mahalla)
