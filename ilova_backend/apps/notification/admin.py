c164651f5eda9d92c43758787a4a73f904e14685
Abdusamad Malikov <sevbofx@gmail.com>
2022-12-17 20:06:05 +0500

    `NotificationAdmin`

--- ilova_backend/apps/notification/admin.py
+++ ilova_backend/apps/notification/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from apps.notification.models import Mahalla
+from apps.notification.models import Mahalla, Notification
 
 
 @admin.register(Mahalla)
@@ -7,3 +7,9 @@ class MahallaAdmin(admin.ModelAdmin):
     list_display = ["district", "mahalla"]
     list_filter = ["district"]
     search_fields = ["district", "mahalla"]
+
+
+@admin.register(Notification)
+class NotificationAdmin(admin.ModelAdmin):
+    list_display = ["title", "description", "image", "date"]
+    search_fields = ["title", "description", "mahalla"]
