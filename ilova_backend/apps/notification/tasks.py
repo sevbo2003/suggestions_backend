5d1147c826134f09f1682dc675e7621ff5ee72c2
Abdusamad Malikov <sevbofx@gmail.com>
2022-12-20 20:39:11 +0500

    `tasks` created, `Send_and_save_notification_to_user`

--- ilova_backend/apps/notification/tasks.py
+++ ilova_backend/apps/notification/tasks.py
@@ -4,7 +4,12 @@ from asgiref.sync import async_to_sync
 from celery import shared_task
 from channels.layers import get_channel_layer
 
+from apps.notification.models import Notification, UserNotification
+from apps.accounts.models import PhoneNumberAbstactUser
+from django.db.models import Q 
+
 channel_layer = get_channel_layer()
+
 
 @shared_task
 def send_notification(group_name, message):
@@ -15,3 +20,16 @@ def send_notification(group_name, message):
             'message': message
         }
     )
+
+@shared_task
+def save_notification_to_notification_users(notification_id, group_name, message):
+    print("Came to task")
+    notification = Notification.objects.get(id=notification_id)
+    users = PhoneNumberAbstactUser.objects.filter(
+        Q(mahallalar__in=notification.mahalla.all()) & Q(is_active=True)
+    )
+    for user in users:
+        UserNotification.objects.create(user=user, notification=notification)
+    send_notification.apply_async(args=[group_name, message])
+    return {'status': 'success'}
+    
