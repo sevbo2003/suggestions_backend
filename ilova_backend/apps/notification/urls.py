65e3d5ca210f6699940b29ea745f73997dcef1a3
Abdusamad Malikov <sevbofx@gmail.com>
2022-12-20 21:01:55 +0500

    `UserNotificationViewSet` `urls`

--- ilova_backend/apps/notification/urls.py
+++ ilova_backend/apps/notification/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from apps.notification.views import NotificationViewSet
+from apps.notification.views import NotificationViewSet, UserNotificationViewSet
 
 router = DefaultRouter()
 router.register('notifications', NotificationViewSet, basename='notification')
+router.register('user-notifications', UserNotificationViewSet, basename='user-notification')
 
 
 urlpatterns = [
