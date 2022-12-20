545f8811baeb49cd1fa483053f90bcf3eac90bec
Abdusamad Malikov <sevbofx@gmail.com>
2022-12-20 20:09:17 +0500

    `NotificationConsumers` updated `notification_message` part

--- ilova_backend/apps/notification/consumers.py
+++ ilova_backend/apps/notification/consumers.py
@@ -7,11 +7,15 @@ from asgiref.sync import sync_to_async
 class NotificationConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         self.group_name = 'notification'
-        await self.channel_layer.group_add(
-            self.group_name,
-            self.channel_name
-        )
-        await self.accept()
+        user = self.scope.get('user', False)
+        if user.is_authenticated:
+            await self.channel_layer.group_add(
+                self.group_name,
+                self.channel_name
+            )
+            await self.accept()
+        else:
+            await self.close()
     
     async def disconnect(self, close_code):
         await self.channel_layer.group_discard(
@@ -22,12 +26,12 @@ class NotificationConsumer(AsyncWebsocketConsumer):
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
-        self.close()
+        await self.close()
 
     @sync_to_async
     def compare(self, message):
         user = self.scope.get('user', False)
-        mahallalar = message['mahalla']
+        mahallalar = [i['id'] for i in message['mahalla']]
         compare = set(user.mahallalar.all().values_list('id', flat = True)) & set(mahallalar)
         if  len(compare) > 0:
             return True
@@ -36,7 +40,5 @@ class NotificationConsumer(AsyncWebsocketConsumer):
     async def notification_message(self, event):
         message = event['message']
         if await self.compare(message):
-            await self.send(text_data=json.dumps({
-                "message": message,
-            }))
+            await self.send(text_data=json.dumps(message))
         
