a03228752539b43d082ac7dd1e11d34dae65a9ce
Abdusamad Malikov <sevbofx@gmail.com>
2022-12-23 15:10:15 +0500

    `asgi.py` changed

--- ilova_backend/config/asgi.py
+++ ilova_backend/config/asgi.py
@@ -1,6 +1,9 @@
 import os
 import django
-from channels.auth import AuthMiddlewareStack
+
+django.setup()
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chat_app.settings')
+
 from .middlewares import JWTAuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 
@@ -8,8 +11,6 @@ from django.core.asgi import get_asgi_application
 
 from apps.chat.routing import websocket_urlpatterns
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chat_app.settings')
-django.setup()
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
